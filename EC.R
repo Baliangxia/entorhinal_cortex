ca970a76b95ad87d24d0b0037eb3c1ba4a393b4c
ChenghaoXia <baliang@bu.edu>
2023-11-03 11:28:54 -0400

    hello

--- EC.R
+++ EC.R
@@ -8,7 +8,7 @@ Raw_EC<- read.csv('AMM20_5DetectionsL2.csv',header = TRUE)
 
 
 gene1<-ggplot(EC) +
-  aes(x = X520) +
+  aes(x = Opal_520) +
   geom_histogram(bins = 50, fill = "green") +
   labs(
     x = "value of Opal 520",
@@ -19,7 +19,7 @@ gene1<-ggplot(EC) +
         axis.title=element_text(size=14))
 
 gene1_log<-ggplot(EC) +
-  aes(x = log(X520)) +
+  aes(x = log(Opal_520)) +
   geom_histogram(bins = 50, fill = "green") +
   labs(
     x = "log value of Opal 520",
@@ -32,7 +32,7 @@ gene1_log<-ggplot(EC) +
 
 
  FI<-ggplot(EC) +
-  aes(x = Distance, y = X520, colour = Class) +
+  aes(x = Distance, y = Opal_520, colour = Class) +
   geom_point(shape = "circle", size = 2) +
   scale_color_hue(direction = 1) +
   labs(
@@ -45,7 +45,7 @@ gene1_log<-ggplot(EC) +
         axis.title=element_text(size=14))
 
 FI_1<-ggplot(EC) +
-  aes(x = Distance, y = X520, colour = Class) +
+  aes(x = Distance, y = Opal_520, colour = Class) +
   geom_point(shape = "circle", size = 4L) +
   scale_color_hue(direction = 1) +
   labs(
@@ -57,3 +57,30 @@ FI_1<-ggplot(EC) +
   theme(axis.text=element_text(size=12),
             axis.title=element_text(size=14))+
   ylim(1, 10)
+
+ggplot(EC) +
+  aes(x = log(Opal_570+1), y = log(Opal_690+1),colour=Class) +
+  geom_point(shape = "circle", size = 1L) +
+  scale_color_hue(direction = 1) +
+  labs(
+    x = "Opal_570",
+    y = "Opal_690",
+    title = "Opal_570 Versus Opal_690 (With Logarithm)"
+  ) +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14))+
+  facet_wrap(vars(Class))
+
+ggplot(EC) +
+  aes(x = Opal_690, y = Distance) +
+  geom_point(shape = "circle", size = 1L) +
+  scale_color_hue(direction = 1) +
+  labs(
+    x = "Opal_690",
+    y = "Distance",
+    title = "Opal_690 Versus Distance"
+    ) +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14))
+
+
