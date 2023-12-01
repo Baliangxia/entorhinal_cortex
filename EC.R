79910befe5709f6685941a4eb71f6da78e5dcd48
ChenghaoXia <baliang@bu.edu>
2023-12-01 10:32:12 -0500

    Nice Work!

--- EC.R
+++ EC.R
@@ -3,10 +3,10 @@ library(tidyverse)
 library(esquisse)
 library(stringr)
 library(gridExtra)
+library(plotly)
 EC<-read.csv("C:/Users/SamXi/OneDrive/Desktop/consulting/entorhinal_cortex/AMM20_5.csv",header = TRUE)
 EC2<-read.csv("C:/Users/SamXi/OneDrive/Desktop/consulting/entorhinal_cortex/AMM12_4.csv",header = TRUE)
 EC3<-read.csv("C:/Users/SamXi/OneDrive/Desktop/consulting/entorhinal_cortex/AMM12_5.csv",header = TRUE)
-
 esquisser(EC)
 
 gene1<-ggplot(EC) +
@@ -234,6 +234,154 @@ b3<-ggplot(EC3) +
         axis.text.x =element_text(angle = 45,vjust = 1,hjust = 1))
 
 grid.arrange(b1,b2,ncol=2)
+
+EC3 %>%
+  filter(Class %in% c("520:570", "520:570:620", "520:570:620:690", "520:570:690")) %>%
+  ggplot() +
+  aes(x = Class, y = log(Opal_620)) +
+  geom_boxplot(fill = "#112446") +
+  theme_minimal()
+
+##shiwen's idea with layer 1
+AMM12_1<-read.csv("C:/Users/SamXi/OneDrive/Desktop/consulting/entorhinal_cortex/AMM12_4DetectionsL2.csv",header=TRUE)
+AMM12_1 <- AMM12_1[c("Name","Cell..Opal.520.mean","Cell..Opal.570.mean", "Cell..Opal.620.mean", "Cell..Opal.690.mean" , "Distance.to.annotation.with.line.µm")]
+colnames(AMM12_1) <- c("Name", "MFI520", "MFI570", "MFI620", "MFI690", "dist")
+
+
+AMM12_1$'IND520' <- grepl("Opal 520", AMM12_1$Name)
+AMM12_1$'IND570' <- grepl("Opal 570", AMM12_1$Name)
+AMM12_1$'IND620' <- grepl("Opal 620", AMM12_1$Name)
+AMM12_1$'IND690' <- grepl("Opal 690", AMM12_1$Name)
+
+AMM12_1 <- AMM12_1[,-1]
+
+AMM12_1 <- filter(AMM12_1, MFI520 != 0 & MFI570 != 0 & MFI620 != 0 & MFI690 != 0)
+
+log_FI <- log(AMM12_1[,1:5])
+
+
+fig_all <- plot_ly(log_FI, x = ~MFI570, y = ~MFI620, z = ~MFI690, marker = list(color = ~MFI520, colorscale = "Rainbow", showscale = TRUE, size = 3))
+fig_all <- fig_all %>% add_markers()
+fig_all <- fig_all %>% layout(title = "520 vs. 570 vs 690,in layer 1",
+                              scene = list(xaxis = list(title = '570'),
+                                           yaxis = list(title = '620'),
+                                           zaxis = list(title = '690')))
+
+fig_all
+
+
+log_FI <- log(filter(AMM12_1, IND520 == T & MFI520 < quantile(AMM12_1$MFI520, 0.95) ))
+fig_520 <- plot_ly(log_FI, x = ~MFI570, y = ~MFI620, z = ~MFI690, marker = list(color = ~MFI520, colorscale = "Rainbow", showscale = TRUE, size = 3))
+fig_520 <- fig_520 %>% add_markers()
+fig_520 <- fig_520 %>% layout(title = "520=T,520 vs. 570 vs 690, in layer 1",
+                              scene = list(xaxis = list(title = '570'),
+                                           yaxis = list(title = '620'),
+                                           zaxis = list(title = '690')))
+fig_520
+
+
+log_FI <- log(filter(AMM12_1,  MFI520 < quantile(AMM12_1$MFI520, 0.95) & dist > quantile(AMM12_1$dist, 0.05)))
+fig_dist <- plot_ly(log_FI, x = ~MFI520, y = ~MFI570, z = ~MFI690, marker = list(color = ~dist, colorscale = "Rainbow", showscale = TRUE, size = 3))
+fig_dist <- fig_dist %>% add_markers()
+fig_dist <- fig_dist %>% layout(title = "520=T, 520 vs. 570 vs 690, in layer 1",
+                                scene = list(xaxis = list(title = '520'), 
+                                             yaxis = list(title = '570'), 
+                                             zaxis = list(title = '690')))
+fig_dist
+## Another layer
+AMM12_2<-read.csv("C:/Users/SamXi/OneDrive/Desktop/consulting/entorhinal_cortex/AMM12_5DetectionsL2.csv",header=TRUE)
+AMM12_2 <- AMM12_2[c("Name","Cell..Opal.520.mean","Cell..Opal.570.mean", "Cell..Opal.620.mean", "Cell..Opal.690.mean" , "Distance.to.annotation.with.line.µm")]
+colnames(AMM12_2) <- c("Name", "MFI520", "MFI570", "MFI620", "MFI690", "dist")
+
+
+AMM12_2$'IND520' <- grepl("Opal 520", AMM12_2$Name)
+AMM12_2$'IND570' <- grepl("Opal 570", AMM12_2$Name)
+AMM12_2$'IND620' <- grepl("Opal 620", AMM12_2$Name)
+AMM12_2$'IND690' <- grepl("Opal 690", AMM12_2$Name)
+
+AMM12_2 <- AMM12_2[,-1]
+
+AMM12_2 <- filter(AMM12_2, MFI520 != 0 & MFI570 != 0 & MFI620 != 0 & MFI690 != 0)
+
+log_FI2 <- log(AMM12_2[,1:5])
+
+
+fig2_all <- plot_ly(log_FI2, x = ~MFI570, y = ~MFI620, z = ~MFI690, marker = list(color = ~MFI520, colorscale = "Rainbow", showscale = TRUE, size = 3))
+fig2_all <- fig2_all %>% add_markers()
+fig2_all <- fig2_all %>% layout(title = "520 vs. 570 vs 690,in layer 2",
+                                scene = list(xaxis = list(title = '570'),
+                                           yaxis = list(title = '620'),
+                                           zaxis = list(title = '690')))
+
+fig2_all
+
+
+log_FI2 <- log(filter(AMM12_2, IND520 == T & MFI520 < quantile(AMM12_2$MFI520, 0.95) ))
+fig2_520 <- plot_ly(log_FI2, x = ~MFI570, y = ~MFI620, z = ~MFI690, marker = list(color = ~MFI520, colorscale = "Rainbow", showscale = TRUE, size = 3))
+fig2_520 <- fig2_520 %>% add_markers()
+fig2_520 <- fig2_520 %>% layout("520=T,520 vs. 570 vs 690, in layer 2",
+                                scene = list(xaxis = list(title = '570'),
+                                           yaxis = list(title = '620'),
+                                           zaxis = list(title = '690')))
+fig2_520
+
+
+log_FI2 <- log(filter(AMM12_2,  MFI520 < quantile(AMM12_2$MFI520, 0.95) & dist > quantile(AMM12_2$dist, 0.05)))
+fig2_dist <- plot_ly(log_FI2, x = ~MFI520, y = ~MFI570, z = ~MFI690, marker = list(color = ~dist, colorscale = "Rainbow", showscale = TRUE, size = 3))
+fig2_dist <- fig2_dist %>% add_markers()
+fig2_dist <- fig2_dist %>% layout(title = "520=T,520 vs. 570 vs 690, in layer 2",
+                                  scene = list(xaxis = list(title = '520'), 
+                                               yaxis = list(title = '570'),
+                                               zaxis = list(title = '690')))
+fig2_dist
+
+## The third layer
+AMM20_1<-read.csv("C:/Users/SamXi/OneDrive/Desktop/consulting/entorhinal_cortex/AMM20_5DetectionsL2.csv",header=TRUE)
+AMM20_1 <- AMM20_1[c("Name","Cell..Opal.520.mean","Cell..Opal.570.mean", "Cell..Opal.620.mean", "Cell..Opal.690.mean" , "Distance.to.annotation.with.line.µm")]
+colnames(AMM20_1) <- c("Name", "MFI520", "MFI570", "MFI620", "MFI690", "dist")
+
+
+AMM20_1$'IND520' <- grepl("Opal 520", AMM20_1$Name)
+AMM20_1$'IND570' <- grepl("Opal 570", AMM20_1$Name)
+AMM20_1$'IND620' <- grepl("Opal 620", AMM20_1$Name)
+AMM20_1$'IND690' <- grepl("Opal 690", AMM20_1$Name)
+
+AMM20_1 <- AMM20_1[,-1]
+
+AMM20_1 <- filter(AMM20_1, MFI520 != 0 & MFI570 != 0 & MFI620 != 0 & MFI690 != 0)
+
+log_FI3 <- log(AMM20_1[,1:5])
+
+
+fig3_all <- plot_ly(log_FI3, x = ~MFI570, y = ~MFI620, z = ~MFI690, marker = list(color = ~MFI520, colorscale = "Rainbow", showscale = TRUE, size = 3))
+fig3_all <- fig3_all %>% add_markers()
+fig3_all <- fig3_all %>% layout(title = "520 vs. 570 vs 690,in layer 3",
+                                scene = list(xaxis = list(title = '570'),
+                                             yaxis = list(title = '620'),
+                                             zaxis = list(title = '690')))
+
+fig3_all
+
+
+log_FI3 <- log(filter(AMM20_1, IND520 == T & MFI520 < quantile(AMM20_1$MFI520, 0.95) ))
+fig3_520 <- plot_ly(log_FI3, x = ~MFI570, y = ~MFI620, z = ~MFI690, marker = list(color = ~MFI520, colorscale = "Rainbow", showscale = TRUE, size = 3))
+fig3_520 <- fig3_520 %>% add_markers()
+fig3_520 <- fig3_520 %>% layout("520=T,520 vs. 570 vs 690, in layer 3",
+                                scene = list(xaxis = list(title = '570'),
+                                             yaxis = list(title = '620'),
+                                             zaxis = list(title = '690')))
+fig3_520
+
+
+log_FI3 <- log(filter(AMM20_1,  MFI520 < quantile(AMM20_1$MFI520, 0.95) & dist > quantile(AMM20_1$dist, 0.05)))
+fig3_dist <- plot_ly(log_FI3, x = ~MFI520, y = ~MFI570, z = ~MFI690, marker = list(color = ~dist, colorscale = "Rainbow", showscale = TRUE, size = 3))
+fig3_dist <- fig3_dist %>% add_markers()
+fig3_dist <- fig3_dist %>% layout(title = "520=T, 520 vs. 570 vs 690, in layer 3",
+                                  scene = list(xaxis = list(title = '520'),
+                                               yaxis = list(title = '570'),
+                                               zaxis = list(title = '690')))
+fig3_dist
+
 
 
 
